78ad6e01bbb469940bc34291196f2637885f45c4
Serge Grigorenko <serge.grigorenko@gmail.com>
2022-07-20 18:56:32 +0300

    Initial commit

---
+++ hardhat.config.ts
@@ -0,0 +1,38 @@
+import * as dotenv from "dotenv";
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+dotenv.config();
+
+const config: HardhatUserConfig = {
+	defaultNetwork: "hardhat",
+	networks: {
+		hardhat: {
+			chainId: 1337,
+		},
+		localhost: {
+			url: "http://127.0.0.1:8545",
+			chainId: 1337,
+		},
+	},
+	solidity: {
+		version: "0.8.9",
+		settings: {
+			optimizer: {
+				enabled: true,
+				runs: 200,
+			},
+		},
+	},
+	paths: {
+		sources: "./contracts",
+		tests: "./tests",
+		cache: "./.cache",
+		artifacts: "./.artifacts",
+	},
+	mocha: {
+		timeout: 40000,
+	},
+};
+
+export default config;
