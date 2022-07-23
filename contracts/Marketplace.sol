0ed795d78f5ea4181082b83b060d04b36d44cae7
Serge Grigorenko <serge.grigorenko@gmail.com>
2022-07-23 09:05:40 +0300

    Implement method to delist token in Marketplace contract

--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -5,14 +5,14 @@ import { IERC721 } from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import { ReentrancyGuard } from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "hardhat/console.sol";
 
+error NotApprovedOperator();
+error NotOwner();
 error PriceMustBeAboveZero();
 error AlreadyListed(address tokenContract, uint256 tokenId);
-error NotOwner();
-error NotApprovedOperator();
+error NotListed(address tokenContract, uint256 tokenId);
 
 // error PriceNotMet(address tokenContract, uint256 tokenId, uint256 price);
 // error ItemNotForSale(address tokenContract, uint256 tokenId);
-// error NotListed(address tokenContract, uint256 tokenId);
 // error NoProceeds();
 
 contract Marketplace is ReentrancyGuard {
@@ -22,6 +22,8 @@ contract Marketplace is ReentrancyGuard {
 	}
 
 	event ItemListed(address indexed seller, address indexed tokenContract, uint256 indexed tokenId, uint256 price);
+	event TokenDelisted(address indexed seller, address indexed tokenContract, uint256 indexed tokenId);
+	// event ItemBought(address indexed buyer, address indexed nftAddress, uint256 indexed tokenId, uint256 price);
 
 	mapping(address => mapping(uint256 => Listing)) private _listings;
 	// mapping(address => uint256) private _proceeds;
@@ -30,6 +32,14 @@ contract Marketplace is ReentrancyGuard {
 		Listing memory listing = _listings[tokenContract][tokenId];
 		if (listing.price > 0) {
 			revert AlreadyListed(tokenContract, tokenId);
+		}
+		_;
+	}
+
+	modifier isListed(address tokenContract, uint256 tokenId) {
+		Listing memory listing = _listings[tokenContract][tokenId];
+		if (listing.price <= 0) {
+			revert NotListed(tokenContract, tokenId);
 		}
 		_;
 	}
@@ -51,7 +61,7 @@ contract Marketplace is ReentrancyGuard {
 		address tokenContract,
 		uint256 tokenId,
 		uint256 price
-	) external notListed(tokenContract, tokenId) isOwner(tokenContract, tokenId, msg.sender) {
+	) external isOwner(tokenContract, tokenId, msg.sender) notListed(tokenContract, tokenId) {
 		if (price <= 0) {
 			revert PriceMustBeAboveZero();
 		}
@@ -63,6 +73,15 @@ contract Marketplace is ReentrancyGuard {
 
 		_listings[tokenContract][tokenId] = Listing(price, msg.sender);
 		emit ItemListed(msg.sender, tokenContract, tokenId, price);
+	}
+
+	function delistToken(address tokenContract, uint256 tokenId)
+		external
+		isOwner(tokenContract, tokenId, msg.sender)
+		isListed(tokenContract, tokenId)
+	{
+		delete (_listings[tokenContract][tokenId]);
+		emit TokenDelisted(msg.sender, tokenContract, tokenId);
 	}
 
 	function getListing(address tokenContract, uint256 tokenId) external view returns (Listing memory) {
--- tests/Marketplace.ts
+++ tests/Marketplace.ts
@@ -71,7 +71,7 @@ describe("Markeplace", function () {
 				await expect(promise).to.be.revertedWithCustomError(marketplace, "PriceMustBeAboveZero");
 			});
 
-			it("Shouldn't fail if called by the token deployer with the right price", async function () {
+			it("Shouldn't fail if called by the token owner with the price above zero wei", async function () {
 				const { marketplace, testItem, user, tokenId, tokenPrice } = await loadFixture(
 					deployMarketplaceFixture,
 				);
@@ -111,6 +111,69 @@ describe("Markeplace", function () {
 				expect(actual).to.contain.keys("price", "seller");
 				expect(actual.price).to.be.equal(tokenPrice);
 				expect(actual.seller).to.be.equal(user.address);
+			});
+		});
+	});
+
+	describe("Listing cancellations", function () {
+		describe("Validations", function () {
+			it("Should revert with the right error if called from an non-owner account", async function () {
+				const { marketplace, testItem, user, user2, tokenId, tokenPrice } = await loadFixture(
+					deployMarketplaceFixture,
+				);
+
+				await testItem.connect(user).approve(marketplace.address, tokenId);
+				await marketplace.connect(user).listItem(testItem.address, tokenId, tokenPrice);
+
+				await testItem.connect(user).approve(user2.address, tokenId);
+
+				const promise = marketplace.connect(user2).delistToken(testItem.address, tokenId);
+				await expect(promise).to.be.revertedWithCustomError(marketplace, "NotOwner");
+			});
+
+			it("Should revert with the right error if the item has not been listed yet", async function () {
+				const { marketplace, testItem, user, tokenId } = await loadFixture(deployMarketplaceFixture);
+
+				await testItem.connect(user).approve(marketplace.address, tokenId);
+
+				const promise = marketplace.connect(user).delistToken(testItem.address, tokenId);
+				await expect(promise)
+					.to.be.revertedWithCustomError(marketplace, "NotListed")
+					.withArgs(testItem.address, tokenId);
+			});
+		});
+
+		describe("Events", function () {
+			it("Should emit an event on a token delisting", async function () {
+				const { marketplace, testItem, user, tokenId, tokenPrice } = await loadFixture(
+					deployMarketplaceFixture,
+				);
+
+				await testItem.connect(user).approve(marketplace.address, tokenId);
+				await marketplace.connect(user).listItem(testItem.address, tokenId, tokenPrice);
+
+				const promise = marketplace.connect(user).delistToken(testItem.address, tokenId);
+				await expect(promise)
+					.to.emit(marketplace, "TokenDelisted")
+					.withArgs(user.address, testItem.address, tokenId);
+			});
+		});
+
+		describe("Actions", function () {
+			it("Should delist the token without returing any data regarding its previous listing", async function () {
+				const { marketplace, testItem, user, tokenId, tokenPrice } = await loadFixture(
+					deployMarketplaceFixture,
+				);
+
+				await testItem.connect(user).approve(marketplace.address, tokenId);
+				await marketplace.connect(user).listItem(testItem.address, tokenId, tokenPrice);
+
+				await marketplace.connect(user).delistToken(testItem.address, tokenId);
+
+				const actual = await marketplace.getListing(testItem.address, tokenId);
+				expect(actual).to.contain.keys("price", "seller");
+				expect(actual.price).to.be.equal(0);
+				expect(actual.seller).to.be.equal(ethers.constants.AddressZero);
 			});
 		});
 	});
